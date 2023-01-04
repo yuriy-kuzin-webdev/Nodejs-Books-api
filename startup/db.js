8c85743af412f5d661835034390edf7f9f7c98ac
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-04 10:32:48 +0200

    fixed mongodb connection

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", false);
 const winston = require('winston');
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/LibraryDatabase', {
+    mongoose.connect('mongodb://127.0.0.1/LibraryDatabase', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(()=> winston.info('Mongo Db Connected'))
