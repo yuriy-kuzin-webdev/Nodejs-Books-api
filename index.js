43eb3097f20ff005b7fcbc62d2a86a8da2d1d7c6
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-04 14:19:24 +0200

    _

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const environments = require("./environments");
-const error = require('./middleware/error');
+const error = require("./middleware/error");
 const winston = require("winston");
 const userRoutes = require("./routes/user-routes");
 const app = express();
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', userRoutes.routesgit)
-app.use(error)
+app.use("/api", userRoutes.routesgit);
+app.use(error);
 
 app.listen(environments.port, () => {
   winston.info("App listening on url: http://localhost:" + environments.port);
